28c9eb9a3739be2f15cdf2f7abad4501c7195d50
Nandeesh Hampiholi <85544202+nandeesh11@users.noreply.github.com>
2023-02-10 18:19:17 +0530

    gadgets.java

---
+++ gadgets.java
@@ -0,0 +1,9 @@
+class Gadgets {
+  void MobilePhones(){
+  System.out.println("Applications");
+  }
+  public static void main(String[] args){
+      Gadgets OppoA5s = new Gadgets();
+      OppoA5s.MobilePhones()
+      }
+}
